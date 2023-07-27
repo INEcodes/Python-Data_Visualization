d08631885c391cac7750e857320c98e67af9b64f
INE Codes <92663561+INE-KRISH@users.noreply.github.com>
2023-07-27 12:39:40 +0530

    Delete homework(date- 15-07-2021).py

--- homework(date- 15-07-2021).py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-agegroup = [10,15,20,25,30,35,12,40,15] 
-plt.hist(agegroup,bins=10)
-plt.xticks([10,20,30,40])
-plt.xlabel('avg. population')
-plt.ylabel('no of people')
