dfa4c761aad7a31d95a976af42790fc5ee745aa3
INE Codes <abhayaps05@gmail.com>
2024-02-28 07:25:11 +0530

    Distribution plot

---
+++ Seaborn/DistPlot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+sns.distplot([0,1,2,3,4,5])#with histogram
+
+sns.distplot([0,1,2,3,4,5], hist=False)#without histogram
+
+iris = sns.load_dataset('iris')
+
+print(iris)
+sns.distplot(iris['sepal_length'])
+plt.show()
+'''
+Distplot stands for distribution plot, it takes as input an array and plots a curve corresponding 
+to the distribution of points in the array.
+'''
